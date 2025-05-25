8b8be34d6e3dad0166c34972420fe46fc1dde10c
Elbra1 <jbolas253@gmail.com>
2025-05-25 10:32:02 -0500

    segundo

--- funciones.js
+++ funciones.js
@@ -5,4 +5,10 @@ function realizarBusqueda() {
     }
   }
 
-  
+
+  function comprar(producto) {
+  const mensaje = `Hola, estoy interesado en el ${producto}. ¿Está disponible?`;
+  const numero = "573122575861";
+  const url = `https://wa.me/${numero}?text=${encodeURIComponent(mensaje)}`;
+  window.open(url, '_blank');
+}
