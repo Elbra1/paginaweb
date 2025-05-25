eb52d514ac16dc66b389f10c62afd8e847d7b63d
Elbra1 <jbolas253@gmail.com>
2025-05-25 11:20:36 -0500

    tercero

--- funciones.js
+++ funciones.js
@@ -1,12 +1,12 @@
 function realizarBusqueda() {
-    const termino = document.getElementById("buscar").value;
-    if (termino.trim() !== "") {
-      window.location.href = `buscar.html?query=${encodeURIComponent(termino)}`;
-    }
+  const termino = document.getElementById("buscar").value;
+  if (termino.trim() !== "") {
+    window.location.href = `buscar.html?query=${encodeURIComponent(termino)}`;
   }
+}
 
 
-  function comprar(producto) {
+function comprar(producto) {
   const mensaje = `Hola, estoy interesado en el ${producto}. ¿Está disponible?`;
   const numero = "573122575861";
   const url = `https://wa.me/${numero}?text=${encodeURIComponent(mensaje)}`;
