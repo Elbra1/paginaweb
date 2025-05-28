67209b1211f35d1594fad8227573bb943be1c374
Elbra1 <jbolas253@gmail.com>
2025-05-28 12:09:48 -0500

    diecisiete

--- funciones.js
+++ funciones.js
@@ -1,11 +1,3 @@
-function realizarBusqueda() {
-  const termino = document.getElementById("buscar").value;
-  if (termino.trim() !== "") {
-    window.location.href = `buscar.html?query=${encodeURIComponent(termino)}`;
-  }
-}
-
-
 function comprar(producto) {
   const mensaje = `Hola, estoy interesado en el ${producto}. ¿Está disponible?`;
   const numero = "573122575861";
