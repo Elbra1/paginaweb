071277de56ff542a50f114c16d249a167b0b96b0
Elbra1 <jbolas253@gmail.com>
2025-06-01 12:27:47 -0500

    mas

--- funciones.js
+++ funciones.js
@@ -11,14 +11,12 @@ function toggleMarca(id, tituloElemento) {
 
   if (seccion.style.display === "flex") {
     seccion.style.display = "none";
-    icono.textContent = "+"; // Cambia a "+" al cerrar
+    icono.textContent = "+"; 
   } else {
     seccion.style.display = "flex";
-    icono.textContent = "−"; // Cambia a "−" al abrir
+    icono.textContent = "−";
   }
 }
-
-
 
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -28,29 +26,7 @@ document.addEventListener("DOMContentLoaded", function () {
   formulario.addEventListener("submit", async function (event) {
     event.preventDefault(); // Evita la recarga
 
-    const datos = new FormData(formulario);
+    alert("Mensaje enviado satisfactoriamente.");
 
-    try {
-      const respuesta = await fetch("https://formspree.io/f/tu-email", {
-        method: "POST",
-        body: datos,
-        headers: {
-          Accept: "application/json",
-        },
-      });
-
-      if (respuesta.ok) {
-        mensaje.style.display = "block";
-        formulario.reset();
-
-        setTimeout(() => {
-          mensaje.style.display = "none";
-        }, 5000);
-      } else {
-        alert("Hubo un error al enviar. Intenta nuevamente.");
-      }
-    } catch (error) {
-      alert("Error de red. Intenta más tarde.");
-    }
   });
 });
