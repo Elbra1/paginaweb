945890fe3d2ad1542f38a9e009668f4e843efa9d
Elbra <jbolas253@gmail.com>
2026-06-18 19:19:40 -0500

    masss

--- funciones.js
+++ funciones.js
@@ -1,60 +1,67 @@
 function comprar(producto) {
-const mensaje = Hola, estoy interesado en el ${producto}. ¿Está disponible?;
-const numero = "573122575861";
-const url = https://wa.me/${numero}?text=${encodeURIComponent(mensaje)};
-window.open(url, "_blank");
+  const mensaje = `Hola, estoy interesado en el ${producto}. ¿Está disponible?`;
+  const numero = "573122575861";
+  const url = `https://wa.me/${numero}?text=${encodeURIComponent(mensaje)}`;
+  window.open(url, "_blank");
 }
 
 function toggleMarca(id, tituloElemento) {
-const seccion = document.getElementById(id);
-const icono = tituloElemento.querySelector(".icono");
+  const seccion = document.getElementById(id);
+  const icono = tituloElemento.querySelector(".icono");
 
-if (seccion.style.display === "flex") {
-seccion.style.display = "none";
-icono.textContent = "+";
-} else {
-seccion.style.display = "flex";
-icono.textContent = "−";
-}
+  if (seccion.style.display === "flex") {
+    seccion.style.display = "none";
+    icono.textContent = "+";
+  } else {
+    seccion.style.display = "flex";
+    icono.textContent = "−";
+  }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
 
-const formulario = document.getElementById("formulario-contacto");
+  const formulario = document.getElementById("formulario-contacto");
 
-if (formulario) {
+  if (formulario) {
 
-formulario.addEventListener("submit", (e) => {
+    formulario.addEventListener("submit", async (e) => {
 
-  e.preventDefault();
+      e.preventDefault();
 
-  const nombre = document.getElementById("nombre").value;
-  const correo = document.getElementById("email").value;
-  const mensaje = document.getElementById("mensaje").value;
+      const datos = {
+        nombre: document.getElementById("nombre").value,
+        correo: document.getElementById("email").value,
+        mensaje: document.getElementById("mensaje").value
+      };
 
-  // 👉 MENSAJE PARA WHATSAPP
-  const texto =
+      try {
 
-`Hola MoviYa:
+        const respuesta = await fetch(
+          "http://localhost:3000/contacto",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json"
+            },
+            body: JSON.stringify(datos)
+          }
+        );
 
-Nombre: ${nombre}
-Correo: ${correo}
+        const resultado = await respuesta.json();
 
-Mensaje:
-${mensaje}`;
+        alert(resultado.mensaje);
 
-  const numero = "573122575861";
+        formulario.reset();
 
-  const url = `https://wa.me/${numero}?text=${encodeURIComponent(texto)}`;
+      } catch (error) {
 
-  // 👉 ABRE WHATSAPP
-  window.open(url, "_blank");
+        console.error(error);
+        alert("Error al enviar el mensaje");
 
-  // 👉 LIMPIA FORMULARIO
-  formulario.reset();
+      }
 
-});
+    });
 
-}
+  }
 
 });
